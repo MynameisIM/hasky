31b81112bad89d9c1dec0028f4b6e4091d796b66
MynameisIM <accforyoutube@mail.ru>
2021-01-16 22:56:08 +0400

    Added header

--- source/blocks/header/header.js
+++ source/blocks/header/header.js
@@ -1,6 +1,32 @@
 export default class Header {
   constructor(parent) {
-    this.header = parent;
-    console.log(this.header);
+    const hamburger = parent.querySelector('.header__hamburger');
+    const menu = parent.querySelector('.header-menu');
+    const list = [].slice.call(parent.querySelectorAll('.header-menu__link-item'));
+
+    if (hamburger && menu) {
+      hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('open');
+        menu.classList.toggle('open');
+      });
+    }
+
+    if (list.length > 0) {
+      list.forEach((item) => {
+        const title = item.querySelector('.header-menu__link-item-wrapper');
+        const dropdown = item.querySelector('.header-menu__link-item-dropdown');
+
+        if (title && dropdown) {
+          title.addEventListener('click', () => {
+            if (item.classList.contains('active')) {
+              item.classList.remove('active');
+            } else {
+              list.forEach(l => l.classList.remove('active'));
+              item.classList.add('active');
+            }
+          });
+        }
+      });
+    }
   }
 }
--- source/entity/index/index.js
+++ source/entity/index/index.js
@@ -1,6 +1,10 @@
 import FirstScreen from '@/blocks/first-screen/first-screen';
+import Header from '@/blocks/header/header';
 
 require('./index.scss');
 
 Array.from(document.querySelectorAll('.first-screen'))
   .forEach(block => block && new FirstScreen(block));
+
+Array.from(document.querySelectorAll('.header'))
+  .forEach(block => block && new Header(block));
