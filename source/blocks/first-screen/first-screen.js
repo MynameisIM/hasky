614cbb929b4216f1b80311ed86bb4c8601cffeed
idem agency <dev@idem.agency>
2021-01-19 15:08:37 +0400

    Added chnages

--- source/blocks/first-screen/first-screen.js
+++ source/blocks/first-screen/first-screen.js
@@ -1,5 +1,24 @@
+import Swiper from 'swiper/swiper-bundle.esm.browser.min';
+
 export default class FirstScreen {
   constructor(parent) {
-    console.log(parent);
+    const container = parent.querySelector('.first-screen__main-container');
+    const thumbSlider = parent.querySelector('.first-screen__thumbs-container');
+    const thumbOptions = {
+      slidesPerView: 1,
+      watchSlidesProgress: true,
+      watchSlidesVisibility: true,
+    };
+    const thumbSw = new Swiper(thumbSlider, thumbOptions);
+    const options = {
+      navigation: {
+        nextEl: parent.querySelector('.first-screen__main-button--next'),
+        prevEl: parent.querySelector('.first-screen__main-button--prev'),
+      },
+      thumbs: {
+        swiper: thumbSw,
+      },
+    };
+    new Swiper(container, options);
   }
 }
