90a69230cc43b6d382e1142ee0f8445a661d2a1d
MynameisIM <accforyoutube@mail.ru>
2021-01-20 00:48:25 +0400

    Added changes

--- source/blocks/first-screen/first-screen.js
+++ source/blocks/first-screen/first-screen.js
@@ -4,10 +4,16 @@ export default class FirstScreen {
   constructor(parent) {
     const container = parent.querySelector('.first-screen__main-container');
     const thumbSlider = parent.querySelector('.first-screen__thumbs-container');
+    const slides = [].slice.call(parent.querySelectorAll('.first-screen__main-slide'));
     const thumbOptions = {
       slidesPerView: 1,
       watchSlidesProgress: true,
       watchSlidesVisibility: true,
+      loop: true,
+      navigation: {
+        nextEl: parent.querySelector('.first-screen__thumbs-button--next'),
+        prevEl: parent.querySelector('.first-screen__thumbs-button--prev'),
+      },
     };
     const thumbSw = new Swiper(thumbSlider, thumbOptions);
     const options = {
@@ -15,10 +21,31 @@ export default class FirstScreen {
         nextEl: parent.querySelector('.first-screen__main-button--next'),
         prevEl: parent.querySelector('.first-screen__main-button--prev'),
       },
+      loop: true,
       thumbs: {
         swiper: thumbSw,
       },
     };
     new Swiper(container, options);
+    const setFontSize = (c, tb) => {
+      let dFont = parseInt(getComputedStyle(tb)['font-size'], 10);
+      if (tb.offsetWidth >= c.offsetWidth) {
+        tb.removeAttribute('style');
+        dFont = 16;
+      }
+      while (tb.offsetWidth < (c.offsetWidth - 30)) {
+        tb.style.fontSize = `${dFont}px`;
+        dFont += 1;
+      }
+    };
+
+    if (slides.length > 0) {
+      slides.forEach((slide) => {
+        const text = slide.querySelector('.first-screen__main-slide-title');
+        setFontSize(slide, text);
+
+        window.addEventListener('resize', setFontSize.bind(this, slide, text));
+      });
+    }
   }
 }
