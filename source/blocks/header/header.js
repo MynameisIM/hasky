ceca8a37210dc0e44104adeea3b57901629e6596
MynameisIM <accforyoutube@mail.ru>
2021-01-21 00:01:32 +0400

    Added changes

--- source/blocks/header/header.js
+++ source/blocks/header/header.js
@@ -4,6 +4,7 @@ export default class Header {
     const menu = parent.querySelector('.header-menu');
     const list = [].slice.call(parent.querySelectorAll('.header-menu__link-item'));
     const page = document.querySelector('.page');
+    const dropdowns = [].slice.call(parent.querySelectorAll('[data-e-parent]'));
 
     if (hamburger && menu) {
       hamburger.addEventListener('click', () => {
@@ -27,6 +28,18 @@ export default class Header {
               list.forEach(l => l.classList.remove('active'));
               item.classList.add('active');
             }
+          });
+        }
+      });
+    }
+
+    if (dropdowns.length > 0) {
+      dropdowns.forEach((el) => {
+        const dd = el.parentElement.querySelector('[data-e-dropdown]');
+
+        if (dd) {
+          el.addEventListener('click', () => {
+            dd.classList.toggle('open');
           });
         }
       });
