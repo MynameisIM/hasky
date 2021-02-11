8816e8d23f15fa45069a7c6595aa86b69d40690e
MynameisIM <accforyoutube@mail.ru>
2021-02-12 01:23:45 +0400

    Added changes

---
+++ source/blocks/custom-select/custom-select.js
@@ -0,0 +1,49 @@
+export default class CustomSelect {
+  constructor(parent) {
+    const container = parent;
+    const select = container.children[0];
+    const optionsItem = container.children[0].options;
+    const dropdown = document.createElement('ul');
+
+    const btn = document.createElement('span');
+    btn.classList.add('custom-select__btn');
+    btn.textContent = optionsItem[0].text;
+    dropdown.classList.add('custom-select__list');
+
+    for (let i = 0; i < optionsItem.length; i += 1) {
+      const dropitem = document.createElement('li');
+      dropitem.classList.add('custom-select__item');
+      dropitem.textContent = optionsItem[i].textContent;
+      if (optionsItem[i].hasAttribute('selected')) {
+        btn.textContent = dropitem.textContent;
+        select.selectedIndex = i;
+        select.dispatchEvent(new window.Event('change'));
+      }
+
+      dropitem.onclick = () => {
+        btn.textContent = dropitem.textContent;
+        dropdown.classList.remove('custom-select__list--opened');
+        container.classList.remove('custom-select--opened');
+        select.selectedIndex = i;
+        select.dispatchEvent(new window.Event('change'));
+      };
+      dropdown.append(dropitem);
+    }
+    dropdown.childNodes[0].style.display = 'none';
+    container.append(btn);
+    container.append(dropdown);
+
+    btn.onclick = () => {
+      dropdown.classList.add('custom-select__list--opened');
+      container.classList.add('custom-select--opened');
+    };
+
+    document.addEventListener('click', (e) => {
+      const { target } = e;
+      if (!container.contains(target)) {
+        dropdown.classList.remove('custom-select__list--opened');
+        container.classList.remove('custom-select--opened');
+      }
+    });
+  }
+}
