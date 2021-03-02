17b3a8f96d4e30c1fe6b50b92c01f614962cc39d
MynameisIM <accforyoutube@mail.ru>
2021-03-03 01:22:25 +0400

    Added changes

---
+++ source/blocks/popup/image/popup-image.js
@@ -0,0 +1,27 @@
+import Swiper from 'swiper/swiper-bundle.esm.browser.min';
+
+export default class PopupImage {
+  constructor(parent) {
+    const container = parent.querySelector('.popup-image__container');
+    const close = parent.querySelector('.popup-image__close');
+    const options = {
+      navigation: {
+        nextEl: parent.querySelector('.popup-image__btn--next'),
+        prevEl: parent.querySelector('.popup-image__btn--prev'),
+      },
+    };
+    window.popupImage = new Swiper(container, options);
+
+    if (close) {
+      close.addEventListener('click', () => {
+        parent.classList.remove('open');
+      });
+    }
+
+    parent.addEventListener('keydown', (e) => {
+      if (e.key === 'Escape') {
+        parent.classList.remove('open');
+      }
+    });
+  }
+}
